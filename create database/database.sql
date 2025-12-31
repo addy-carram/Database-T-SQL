fb626c880b3751bb34fe42899b453a1dec6ea785
ady <addycarram@gmail.com>
2025-12-31 17:46:35 +0200

    create database

---
+++ create database/database.sql
@@ -0,0 +1,11 @@
+-- ========================================
+-- CAR SALES DATABASE 
+-- ========================================
+-- This database manages automobile sales, tracking information about products,
+-- customers, employees, contracts, warranties, and insurance companies.
+
+CREATE DATABASE CarSales;
+GO
+
+USE CarSales;
+GO
